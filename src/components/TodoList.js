ea06584e48a1b43f8bcb59031fb8a91f8a01628f
Erik McComb <erik@erikmccomb.com>
2017-09-19 11:46:29 -0600

    Corrected TodoList output

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 // We need to pass {todos} props, and they will be passed in a nd rendered.
 const TodoList = ({ todos }) => (
   <ul>
-    { todos.map( (t, i) => <li key={i}>{t}></li> ) }
+    { todos.map( (t, i) => <li key={i}>{t}</li> ) }
   </ul>
 )
 
