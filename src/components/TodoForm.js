85084517176c8a358350be90ea46b8ca51089fa4
Erik McComb <erik@erikmccomb.com>
2017-09-19 11:31:13 -0600

    todo form takes in user input and adds it to the virtual dom,a nd updates the dom

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,5 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const TodoForm = () => <div>TodoForm</div>
+// Class because the form will have state
+class TodoForm extends React.Component {
+  state = {name: ''}
 
-export default TodoForm;
+  onChange = (e) => {
+    this.setState({name: e.target.value});
+  }
+  handleSubmit = (e) => {
+    // stop the form from behaving as a form, and use the below JS instead.
+    e.preventDefault();
+    // Get name from input
+    let name=this.state.name;
+    // Update redux store
+    this.props.dispatch({ type: 'ADD_TODO', todo: name });
+    // Clear input
+    this.setState({name: ''})
+  }
+
+  render() {
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <input 
+          value ={this.state.name} 
+          onChange={this.onChange} 
+        />
+      </form>
+    )
+  }
+}
+
+export default connect()(TodoForm);
